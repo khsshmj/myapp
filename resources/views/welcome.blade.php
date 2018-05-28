41ac8ce29b6ac575a7b8b3f3dac9727be0f9b18c
khsshmj <khs1157@gmail.com>
2018-05-28 12:31:18 +0900

    뷰와 데이터 바인딩

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,3 @@
-<h1><?php echo isset($greeting)? "{$greeting}" : 'Hello ';?><?php echo $name;?></h1>
+<!--HTML 주석 안에서 {{ $name }}을(를) 출력합니다.-->
+{{--블레이드 주석 안에서 {{ $name }}을(를) 출력합니다.--}}
+<h1>{{ $greeting or 'Hello ' }} {{ $name or '' }}</h1>
